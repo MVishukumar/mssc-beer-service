a67399af4c23bc03e210b6c06a88d129f0325767
MVishukumar <beingvishu@gmail.com>
2021-10-13 23:47:50 +0530

    Added JPA entity Beer. Important.

--- src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -1,12 +1,14 @@
 package guru.springframework.msscbeerservice.web.controller;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@Slf4j
 @RequestMapping("/api/v1/beer")
 @RestController
 public class BeerController {
@@ -14,6 +16,7 @@ public class BeerController {
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
 
+        System.out.println("Hit get beer by id.");
         //todo
         return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
     }
@@ -21,6 +24,8 @@ public class BeerController {
     @PostMapping
     public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
 
+        log.debug("Save New Beer method hit.");
+        System.out.println("Hit save new beer");
         //todo
         return new ResponseEntity(HttpStatus.CREATED);
     }
