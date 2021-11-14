c1308bc59417c9d4bd66a30c0d253571ad5970a5
vishukumarm <beingvishu@gmail.com>
2021-11-14 20:55:41 +0530

    Configure HTTP Basic Auth for RESTTemplate.

--- src/main/java/guru/springframework/msscbeerservice/service/inventory/BeerInventoryServiceRestTemplateImpl.java
+++ src/main/java/guru/springframework/msscbeerservice/service/inventory/BeerInventoryServiceRestTemplateImpl.java
@@ -2,6 +2,7 @@ package guru.springframework.msscbeerservice.service.inventory;
 
 import guru.springframework.msscbeerservice.service.inventory.model.BeerInventoryDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Profile;
@@ -31,8 +32,12 @@ public class BeerInventoryServiceRestTemplateImpl implements BeerInventoryServic
         this.beerInventoryServiceHost = beerInventoryServiceHost;
     }
 
-    public BeerInventoryServiceRestTemplateImpl(RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplate = restTemplateBuilder.build();
+    public BeerInventoryServiceRestTemplateImpl(RestTemplateBuilder restTemplateBuilder,
+                                                @Value("${sfg.brewery.inventory-user}") String inventoryUser,
+                                                @Value("${sfg.brewery.inventory-password}") String inventoryPassword) {
+        this.restTemplate = restTemplateBuilder
+                .basicAuthentication(inventoryUser, inventoryPassword)
+                .build();
     }
 
     @Override
